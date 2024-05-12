548ad70662b90e901186db0f245acaf9a03dffad
Luis Paulo França <franca.luiz.paulo.777@gmail.com>
2024-05-12 09:34:34 -0300

    7 - Cadastro de categorias realizado

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const category = require('../models/categoryModel')
+const slugify = require('slugify')
 // Rota GET
 router.get('/', (req, res) => {
     res.render('index'); // exibe a ejs view engine "index.ejs"
@@ -8,6 +9,19 @@ router.get('/', (req, res) => {
 
 // Rotas de Categorias
 router.get('/admin/categories/new',(req,res) => {res.render('admin/categories/new')})
+router.post('/categories/save',(req,res) => {
+    let title = req.body.title
+    if(title != undefined){
+        category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() =>{
+            res.redirect('/')
+        })
+    }else{
+        res.redirect('admin/categories/new')}
+    }
+)
 
 // Rotas de artigos
 router.get('/articles', (req, res) => {
